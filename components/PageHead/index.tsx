15adfdcd9af1a78382afe42c6703b3bf423e2917
Cypher Moon <temperaturecaleb@gmail.com>
2023-01-07 18:44:51 +0100

    changed the meta description

--- components/PageHead/index.tsx
+++ components/PageHead/index.tsx
@@ -2,12 +2,14 @@ import Head from "next/head"
 import { headType } from "../../types"
 
 
-const PageHead = ({title} :headType) => {
+const PageHead = ({ title }: headType) => {
   return (
-        <Head>
-        <title>{title}</title>
-        <meta name="description" content="Kelvin's portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content="Kelvin's portfolio" />
+      <meta name="keyword" content="cypher moon, Kelvin's portfolio, cypher-moon portfolio " />
+      <meta name="author" content="Kelvin (cypher moon)" />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
   )
 }
