a0f918e457e5deb90cf5628458a901f3e5edf6db
Cypher Moon <temperaturecaleb@gmail.com>
2023-01-07 21:16:59 +0100

    seperated the loading screen components

--- components/LoadingScreen/index.tsx
+++ components/LoadingScreen/index.tsx
@@ -1,5 +1,7 @@
 import gsap from 'gsap'
 import React, { useEffect, useRef } from 'react'
+import MoonBody from './MoonBody'
+import MoonCrescent from './MoonCrescent'
 
 const LoadingScreen = () => {
     const rootComp = useRef<HTMLDivElement>(null)
@@ -34,14 +36,8 @@ const LoadingScreen = () => {
             <div id='moon_container' className='
                     absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
                     grid grid-cols-2 justify-items-center items-center p-4 w-8/12'>
-
-                <div className="w-16 h-16 rounded-full shadow-moon-outline shadow-slate-600 dark:shadow-blue-300"></div>
-
-                <div className='w-20 h-20 relative z-10 bg-slate-600 dark:bg-blue-300 rounded-full shadow-lg clip-moon'>
-                    <div className="details"></div>
-                    <div className="details top-8 left-3 w-5 h-5"></div>
-                    <div className="details top-11 left-8 w-4 h-4"></div>
-                </div>
+                <MoonCrescent />
+                <MoonBody />
             </div>
         </div>
     )
