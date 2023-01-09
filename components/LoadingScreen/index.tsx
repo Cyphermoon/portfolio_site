748436e057a84544fae794c8a5be7b763ee0898e
Cypher Moon <temperaturecaleb@gmail.com>
2023-01-09 14:23:14 +0100

    optimized loading screen animation

--- components/LoadingScreen/index.tsx
+++ components/LoadingScreen/index.tsx
@@ -9,6 +9,28 @@ const LoadingScreen = () => {
     const t1 = useRef<GSAPTimeline>()
 
     useEffect(() => {
+        const animationProps = {
+            moon: {
+                start: "210px",
+                end: "300px",
+                endDuration: 1.3
+            },
+            container: {
+                rotate: "180",
+                duration: .8,
+            },
+            split_screen: {
+                travelDistance: "100%",
+                duration: 1.5
+            },
+
+            crescentFade: {
+                opacity: "0",
+                duration: 1
+            }
+
+        }
+
         const ctx = gsap.context(() => {
             t1.current = gsap.timeline({
                 defaults: {
@@ -16,28 +38,36 @@ const LoadingScreen = () => {
                     duration: 1,
                 }
             })
-                .to("#moon_container", {
-                    width: "85px",
+                .to("#moon_crescent", {     // pull crescent to the right
+                    translateX: `${animationProps.moon.start}`,
                 })
-                .to("#moon_container", {
-                    rotation: 180,
-                    duration: .8
-                }, "<.4")
-                .to("#moon_container", {
-                    width: "300%",
-                    duration: 1.5
-                }, "<.35")
-                .to("div[data-animation='pull-left']", {
-                    translateX: "-100%",
-                    duration: 1.5,
-                }, "<.1")
-                .to("div[data-animation='pull-right']", {
-                    translateX: "+100%",
-                    duration: 1.5
+                .to("#moon_body", {         // pull body to the left
+                    translateX: `-${animationProps.moon.start}`,
                 }, "<")
-                .to("#moon_container > *", {
-                    opacity: "0",
-                    duration: 1
+                .to("#moon_container", {    // rotate both element
+                    rotation: animationProps.container.rotate,
+                    duration: animationProps.container.duration
+                }, "<.5")
+                .to("#moon_crescent", {     // pull crescent to the left
+                    translateX: `-${animationProps.moon.end}`,
+                    duration: animationProps.moon.endDuration,
+                }, "<.55")
+                .to("#moon_body", {         // pull body to the right
+                    translateX: `${animationProps.moon.end}`,
+                    duration: animationProps.moon.endDuration,
+
+                }, "<")
+                .to("div[data-animation='pull-left']", {    //split screen left
+                    translateX: `-${animationProps.split_screen.travelDistance}`,
+                    duration: animationProps.split_screen.duration,
+                }, "<.11")
+                .to("div[data-animation='pull-right']", {  // split screen right
+                    translateX: `${animationProps.split_screen.travelDistance}`,
+                    duration: animationProps.split_screen.duration
+                }, "<")
+                .to("#moon_container > *", {    // fade the crescent and moon
+                    opacity: `${animationProps.crescentFade.opacity}`,
+                    duration: animationProps.crescentFade.duration
                 }, "<")
         }, rootComp)
 
@@ -45,7 +75,7 @@ const LoadingScreen = () => {
     }, [])
 
     return (
-        <div ref={rootComp} className='w-screen overflow-hidden relative h-screen flex'>
+        <div ref={rootComp} className='w-screen bg-transparent overflow-hidden relative h-screen flex'>
 
             <SplitScreen animationType="pull-left" />
             <SplitScreen animationType="pull-right" />
