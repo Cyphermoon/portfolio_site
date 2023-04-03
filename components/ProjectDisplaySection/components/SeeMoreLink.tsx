e6b9b4f17f466e42d5eac5b58143ca6f8e27baf5
Cypher Moon <temperaturecaleb@gmail.com>
2023-04-03 13:35:12 +0100

    re-styled the see more hover card

--- components/ProjectDisplaySection/components/SeeMoreLink.tsx
+++ components/ProjectDisplaySection/components/SeeMoreLink.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { useTiltEffect } from '../../../hooks/index.util'
+import { IoMdEye } from 'react-icons/io'
 
 const SeeMoreLink = () => {
     const { tiltCard, removeTiltEffect } = useTiltEffect()
@@ -9,14 +10,13 @@ const SeeMoreLink = () => {
     return (
         <Link href="/projects">
             <div
-                className='min-h-[490px] w-full sm:w-72 sm:min-h-[384px] md:min-h-[512px] md:w-[365px] lg:w-96 bg-slate-400 
-                grid place-items-center'
+                className='min-h-[490px] w-full sm:w-72 sm:min-h-[384px] md:min-h-[512px] md:w-[365px] lg:w-96 grid place-items-center cursor-pointer bg-gradient-to-br from-slate-400 via-slate-500 to-slate-600 dark:from-sky-500  dark:to-sky-600'
                 onMouseMove={tiltCard}
                 onMouseOut={removeTiltEffect}>
 
-                <div className='flex flex-col items-center space-y-3 p-4 rounded-2xl bg-slate-300 w-1/2'>
-                    <Image src={"/icons/see_more_icon.svg"} alt="see more icon" width={80} height={80} />
-                    <span className='text-slate-700 text-sm '>see more projects</span>
+                <div className='flex flex-col items-center space-y-3 p-4 rounded-2xl shadow-xl w-1/2 dark:text-blue-200 text-slate-300'>
+                    <IoMdEye className='text-8xl' />
+                    <span className='text--300 text-sm '>see all projects</span>
                 </div>
 
             </div>
