c209d6a1af3de17fa5725a03f415504a964e308f
Cypher Moon <temperaturecaleb@gmail.com>
2023-04-04 22:24:52 +0100

    made functionalities image contain

--- components/FeatureContent/index.tsx
+++ components/FeatureContent/index.tsx
@@ -12,7 +12,7 @@ export const FeatureContentImage = ({ imageURL, altContent }: featureContentImag
       className='relative drop-shadow-2xl shadow-slate-900 dark:drop-shadow-none w-full lg:w-5/12 h-[365px] bg-slate-600 dark:bg-slate-700'
       style={{ transform: "perspective(1000px)" }}
       onMouseMove={tiltCard} onMouseLeave={removeTiltEffect}>
-      <Image src={imageURL ?? ""} alt={altContent} layout='fill' objectFit='cover' />
+      <Image src={imageURL ?? ""} alt={altContent} layout='fill' objectFit='contain' />
     </figure>
   )
 }
