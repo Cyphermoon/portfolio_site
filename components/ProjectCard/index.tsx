c77947050f6054485c06956c7442b9baf1edcc36
Cypher Moon <temperaturecaleb@gmail.com>
2023-07-10 10:03:48 +0100

    add pointers to the projectCard

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -10,17 +10,21 @@ const ProjectCard = ({ title, description, imageURL, projectId }: projectCardTyp
 
     return (
         <Link href={`/project/${encodeURIComponent(projectId)}`}>
-            <div className='min-h-[490px] w-full sm:w-72 sm:min-h-[384px] md:min-h-[512px] md:w-[365px] lg:w-96 bg-slate-300 relative before:w-full cursor-pointer
+            <div className='min-h-[490px] w-full sm:w-72 sm:min-h-[384px] md:min-h-[512px] md:w-[365px] lg:w-96 bg-slate-300 relative before:w-full cursor-pointer group
                 before-h-full before:transition-opacity before:duration-100ms
                 before:inset-0 before:z-10 before:absolute before:bg-slate-900 dark:shadow-2xl dark:shadow-slate-700 md:dark:shadow-slate-800 before:opacity-60 dark:before:opacity-60
                 md:dark:before:opacity-70 dark:bg-slate-800
                 before:hover:opacity-80 dark:before:hover:opacity-50'
                 onMouseMove={tiltCard} onMouseOut={removeTiltEffect}>
-                <Image src={imageURL} alt="project image 1" layout='fill' objectFit='cover' />
+                <Image src={imageURL} alt={`${title} image`} layout='fill' objectFit='cover' />
 
                 <div className="absolute top-3 left-3 z-10 text-gray-100 dark:text-slate-100 space-y-2">
                     <h3 className='capitalize'>{title}</h3>
                     <p className='text-label_md normal-case'>{description}</p>
+                </div>
+
+                <div className='absolute bottom-3 right-3 z-10 text-gray-300 dark:text-slate-400 lg:dark:text-slate-300 space-y-2 lg:hidden lg:group-hover:block'>
+                    <p>see details</p>
                 </div>
             </div>
         </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -63,19 +63,6 @@ const Home: NextPage<homePageType> = (
 
 
             <div className="w-full self-center lg:w-6/12 flex flex-col  sm:flex-row justify-between sm:space-x-4 space-y-4 sm:space-y-0">
-              <Link href={landing_section?.contact_btn?.href} passHref >
-                <a className="capitalize group block relative shadow-lg shadow-blue-400 dark:shadow-gray-900 w-full md:w-3/4 bg-blue-500 rounded-full px-10 sm:px-14 py-4 text-base text-white hover:text-slate-200 dark:text-slate-200 sm:mr-7
-                ">
-                  <span className='w-full block h-full absolute rounded-full bg-red-300 top-0 left-0
-                    scale-y-1 scale-x-0 group-hover:scale-x-100 origin-left  
-                    transition-all duration-150 ease-linear z-0' />
-
-                  <span className='block z-10 relative'>
-                    {landing_section.contact_btn.display_text}
-                  </span>
-                </a>
-              </Link>
-
               <Link href={landing_section?.get_resume_btn?.href} passHref>
                 <a target={'_blank'} className="capitalize group block relative dark:shadow-lg  dark:shadow-gray-900 w-full md:w-3/4 bg-transparent border-2 border-blue-400 rounded-full px-10 sm:px-14 py-4 text-base text-blue-500 dark:text-slate-200 hover:text-slate-200 sm:mr-7">
 
@@ -85,6 +72,19 @@ const Home: NextPage<homePageType> = (
 
                   <span className='block z-10 relative'>
                     {landing_section.get_resume_btn.display_text}
+                  </span>
+                </a>
+              </Link>
+
+              <Link href={landing_section?.contact_btn?.href} passHref >
+                <a className="capitalize group block relative shadow-lg shadow-blue-400 dark:shadow-gray-900 w-full md:w-3/4 bg-blue-500 rounded-full px-10 sm:px-14 py-4 text-base text-white hover:text-slate-200 dark:text-slate-200 sm:mr-7
+                ">
+                  <span className='w-full block h-full absolute rounded-full bg-red-300 top-0 left-0
+                    scale-y-1 scale-x-0 group-hover:scale-x-100 origin-left  
+                    transition-all duration-150 ease-linear z-0' />
+
+                  <span className='block z-10 relative'>
+                    {landing_section.contact_btn.display_text}
                   </span>
                 </a>
               </Link>
