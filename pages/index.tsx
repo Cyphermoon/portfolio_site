3f43ae84bad4fc9f1bd5e0143dc14dd00d0663ec
Cypher Moon <temperaturecaleb@gmail.com>
2024-06-17 10:12:19 +0100

    chore: Make button positions more UX friendly by switching positions

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,38 +70,41 @@ const Home: NextPage<homePageType> = (
             <p className="text-title_sm">{landing_section.elongated_text}</p>
 
 
-            <div className="w-full self-center lg:w-6/12 flex flex-col  sm:flex-row justify-between sm:space-x-4 space-y-4 sm:space-y-0">
-              <Link href={landing_section?.get_resume_btn?.href} passHref>
-                <a target={'_blank'} className="capitalize group block relative dark:shadow-lg  dark:shadow-gray-900 w-full md:w-3/4 bg-transparent border-2 border-blue-400 rounded-full px-10 sm:px-14 py-4 text-base text-blue-500 dark:text-slate-200 hover:text-slate-200 sm:mr-7">
+            <div className="w-full self-center lg:w-6/12 flex flex-col sm:flex-row-reverse justify-between sm:space-x-4 space-y-4 sm:space-y-0">
 
-                  <span className='w-full block h-full absolute rounded-full bg-blue-400 top-0 left-0
-                    scale-y-1 scale-x-0 group-hover:scale-x-100 origin-left  
-                    transition-all duration-150 ease-linear z-0' />
-
-                  <span className='block z-10 relative'>
-                    {landing_section.get_resume_btn.display_text}
-                  </span>
-                </a>
-              </Link>
-
-              <Link href={landing_section?.contact_btn?.href} passHref >
-                <a className="capitalize group block relative shadow-lg shadow-blue-400 dark:shadow-gray-900 w-full md:w-3/4 bg-blue-500 rounded-full px-10 sm:px-14 py-4 text-base text-white hover:text-slate-200 dark:text-slate-200 sm:mr-7
+              <Link href={landing_section?.get_resume_btn?.href} passHref >
+                <a className="capitalize group block relative shadow-lg shadow-blue-400 dark:shadow-gray-900 w-full md:w-3/4 bg-blue-500 rounded-full px-10 sm:px-14 py-4 text-base text-white hover:text-slate-200 dark:text-slate-200 sm:ml-5
                 ">
                   <span className='w-full block h-full absolute rounded-full bg-red-300 top-0 left-0
                     scale-y-1 scale-x-0 group-hover:scale-x-100 origin-left  
                     transition-all duration-150 ease-linear z-0' />
 
                   <span className='block z-10 relative'>
+                    {landing_section.get_resume_btn.display_text}
+
+                  </span>
+                </a>
+              </Link>
+
+              <Link href={landing_section?.contact_btn?.href} passHref>
+                <a target={'_blank'} className="capitalize group block relative dark:shadow-lg  dark:shadow-gray-900 w-full md:w-3/4 bg-transparent border-2 border-blue-400 rounded-full px-10 sm:px-14 py-4 text-base text-blue-500 dark:text-slate-200 hover:text-slate-200">
+
+                  <span className='w-full block h-full absolute rounded-full bg-blue-400 top-0 left-0
+                    scale-y-1 scale-x-0 group-hover:scale-x-100 origin-left  
+                    transition-all duration-150 ease-linear z-0' />
+
+                  <span className='block z-10 relative'>
                     {landing_section.contact_btn.display_text}
+
                   </span>
                 </a>
               </Link>
             </div>
           </div>
         </Header>
+        <SkillDisplaySection addAnimation={addAnimation} skill_list={skill_list} />
         <AboutSection addAnimation={addAnimation} about_data={about_data} />
         <ProjectDisplaySection addAnimation={addAnimation} projects={projects} />
-        <SkillDisplaySection addAnimation={addAnimation} skill_list={skill_list} />
         <ContactSection addAnimation={addAnimation} social_medias={social_medias} />
       </Container>
     </Background>
