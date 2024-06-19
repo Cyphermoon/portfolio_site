af8e77bd5c2e7a55c384e278cd6f53fe18092128
Cypher Moon <temperaturecaleb@gmail.com>
2024-06-19 10:18:42 +0100

    Add a bacground layer to the profile photo in the about section

--- components/AboutSection/index.tsx
+++ components/AboutSection/index.tsx
@@ -35,9 +35,12 @@ const AboutSection = ({ about_data, addAnimation }: aboutSectionType) => {
             <h2 className='dark:text-slate-300 hidden lg:block'>About</h2>
             <div className='p-4 lg:py-12 lg:px-8 mx-auto rounded-xl bg-slate-200 dark:bg-slate-800 w-full flex flex-col lg:flex-row lg:justify-between lg:items-center lg:space-x-10 '>
                 <div className='lg:w-5/12 flex flex-col items-center justify-between space-y-8 relative'>
-                    <figure className='absolute -top-[100px] lg:top-0 lg:relative w-[200px] h-[200px] lg:w-[290px] lg:h-[290px] rounded-full overflow-hidden'>
-                        <Image src={about_data.profilePhoto.url} layout='fill' alt={about_data.profilePhoto.alt} objectFit='cover' />
-                    </figure>
+                    <div className='flex flex-col items-center justify-between absolute -top-[100px] lg:top-0 lg:relative w-[220px] h-[220px] lg:w-[290px] lg:h-[290px] rounded-full bg-slate-600 lg:bg-transparent p-2'>
+                        <figure className='absolute top-[50%] -translate-y-1/2 lg:translate-y-0 lg:top-0 lg:relative w-[200px] h-[200px] lg:w-[290px] lg:h-[290px] rounded-full overflow-hidden'>
+                            <Image src={about_data.profilePhoto.url} layout='fill' alt={about_data.profilePhoto.alt} objectFit='cover' />
+                        </figure>
+                    </div>
+
                 </div>
                 <h2 className='dark:text-slate-300 block lg:hidden text-center mt-[150px] lg:mt-0 mb-3'>About me</h2>
 
