c3d1acc7d538aac5ebd89037bd91c523929cbb92
Cypher Moon <temperaturecaleb@gmail.com>
2024-06-19 12:37:31 +0100

    fix: Fix UI bug that makes screen stuck at the top

--- components/ContactSection/index.tsx
+++ components/ContactSection/index.tsx
@@ -5,7 +5,7 @@ import SocialMediaDisplay from '../SocialMediaDisplay'
 const ContactSection = ({ social_medias, addAnimation }: contactSectionType) => {
     const sectionHeight = '800px'
     return (
-        <section className={`space-y-8 lg:h-[${sectionHeight}]`} id='contact_section'>
+        <section className={`space-y-8 lg:h-[${sectionHeight}] overflow-hidden`} id='contact_section'>
             <h2 className='dark:text-slate-300 text-center mb-10'>Contact me</h2>
             <div className='w-full flex flex-col md:flex-row items-start justify-between '>
                 <ContactForm addAnimation={addAnimation} />
